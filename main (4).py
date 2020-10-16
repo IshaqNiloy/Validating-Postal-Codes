50de1d3fdb7db66ceb8fb03cf1efdd19cd1ab5bc
Ishaq Ali <ishaqniloy1996@gmail.com>
2020-10-16 10:40:01 +0600

    Added the documentation

--- main (4).py
+++ main (4).py
@@ -1,6 +1,11 @@
 regex_integer_in_range = r"[0-9]{6}$"	# Do not delete 'r'.
 regex_alternating_repetitive_digit_pair = r"([0-9])(?=\d\1)"	# Do not delete 'r'.
 
+#(\d): Match and capture a digit in group #1
+#(?=: Start lookahead
+#\d: Match any digit
+#\1: Back-reference to captured group #1
+#): End lookahead
 
 import re
 P = input()
